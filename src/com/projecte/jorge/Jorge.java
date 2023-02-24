c7d3e39c69728e667783fac77b94fbac8cc7f1cb
JC <optenty123456@gmail.com>
2023-02-24 13:19:21 +0100

    Correcció, Projecte Base

---
+++ src/com/projecte/jorge/Jorge.java
@@ -0,0 +1,8 @@
+package com.projecte.jorge;
+
+public class Jorge {
+    private static final String NOM="Jorge";
+    public void MostrarNom(){
+        System.out.println("Hola, este es el projecte de "+this.NOM);
+    }
+}
