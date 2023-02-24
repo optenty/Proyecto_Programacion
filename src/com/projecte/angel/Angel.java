f001616716fb84d262c1059848bb4715a5ba3b43
angelJT <trevol20019@gmail.com>
2023-02-24 13:16:11 +0100

    angel
    angel java

---
+++ src/com/projecte/angel/Angel.java
@@ -0,0 +1,10 @@
+package com.projecte.angel;
+
+public class Angel {
+
+	public static void Nom() {
+		System.out.println("Hola, este es el projecte de Angel");
+
+	}
+
+}
--- src/com/projecte/main/ProgramaPrincipal.java
+++ src/com/projecte/main/ProgramaPrincipal.java
@@ -1,11 +1,17 @@
 package com.projecte.main;
 import com.projecte.jorge.Jorge;
+import com.projecte.daniel.Daniel;
+import com.projecte.angel.Angel;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		Jorge p1 = new Jorge();
 		p1.Nom();
+		Daniel p2 = new Daniel();
+		p2.Nom();
+		Angel p3 = new Angel();
+		p3.Nom();
 	}
 
 }
