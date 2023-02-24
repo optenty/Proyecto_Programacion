f9f82578fbd58abb27ce4d724932d2372d803698
JC <optenty123456@gmail.com>
2023-02-24 12:58:13 +0100

    Projecte INICIAL-- BASE

---
+++ src/com/projecte/jorge/Jorge.java
@@ -0,0 +1,7 @@
+package com.projecte.jorge;
+
+public class Jorge {
+    public static void Nom(){
+        System.out.println("Hola, este es el projecte de Jorge");
+    }
+}
---
+++ src/com/projecte/main/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package com.projecte.main;
+import com.projecte.jorge.Jorge;
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		Jorge p1 = new Jorge();
+		p1.Nom();
+	}
+
+}
